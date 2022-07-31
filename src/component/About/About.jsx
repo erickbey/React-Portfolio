a146da97c898071ea04274710164d3eae81dcb3f
Erick Bey <erickbey10@gmail.com>
2022-07-31 00:24:08 -0400

    added an about me component

--- src/component/About/About.jsx
+++ src/component/About/About.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import React from 'react';
+import './About.css';
+import Me from '../../assets/standing-on-mountain(1).jpg';
+
 
 function About() {
   return (
-    <section id='about'></section>
+    <section id='about'>
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={Me} alt='standing on a mountain' />
+          </div>
+        </div>
+
+        <div className='about__content'>
+          <p>
+            Hello! My name is Erick Bey and I am a fullstack web developer with a passion learn new things and grow my skills. I enjoy getting out in nature, playing disc golf, spending time with my family and dogs, and creating new things.
+            I am ready to join a team to contiribute my skills and grow as we make progress towards our goals.
+          </p>
+          <a href='#contact' className='btn btn-primary'>
+            Let's Talk
+          </a>
+        </div>
+      </div>
+    </section>
   )
 }
 
--- src/component/Nav/Nav.jsx
+++ src/component/Nav/Nav.jsx
@@ -12,13 +12,43 @@ function Nav() {
 
   return (
     <nav>
-        <a href="#" onClick={() => setActive('#')} className={active === '#' ? 'active' : ''}><FaHome /></a>
-        <a href="#about" onClick={() => setActive('#about')} className={active === '#about' ? 'active' : ''}><FaUser /></a>
-        <a href="#experience" onClick={() => setActive('#experience')} className={active === '#experience' ? 'active' : ''}><FaBook /></a>
-        <a href="#projects" onClick={() => setActive('#projects')} className={active === '#projects' ? 'active' : ''}><HiCode /></a>
-        <a href="#contact" onClick={() => setActive('#contact')} className={active === '#contact' ? 'active' : ''}><AiFillMessage /></a>
+      <a
+        href="#"
+        onClick={() => setActive("#")}
+        className={active === "#" ? "active" : ""}
+      >
+        <FaHome />
+      </a>
+      <a
+        href="#about"
+        onClick={() => setActive("#about")}
+        className={active === "#about" ? "active" : ""}
+      >
+        <FaUser />
+      </a>
+      <a
+        href="#experience"
+        onClick={() => setActive("#experience")}
+        className={active === "#experience" ? "active" : ""}
+      >
+        <FaBook />
+      </a>
+      <a
+        href="#projects"
+        onClick={() => setActive("#projects")}
+        className={active === "#projects" ? "active" : ""}
+      >
+        <HiCode />
+      </a>
+      <a
+        href="#contact"
+        onClick={() => setActive("#contact")}
+        className={active === "#contact" ? "active" : ""}
+      >
+        <AiFillMessage />
+      </a>
     </nav>
-  )
+  );
 }
 
 export default Nav
