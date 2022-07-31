c585f55caed4785858ba6735d674c62fb0aec8b8
Erick Bey <erickbey10@gmail.com>
2022-07-31 04:33:50 -0400

    added a footer component

--- src/component/Footer/Footer.jsx
+++ src/component/Footer/Footer.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import React from 'react';
+import './Footer.css';
+import { BsLinkedin } from 'react-icons/bs';
+import { BsGithub } from 'react-icons/bs'
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href="#" className="footer__logo">Erick Bey</a>
+
+      <ul className="permalinks">
+        <li><a href="#">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#projects">Projects</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+
+      <div className="footer__socials">
+        <a href='https://www.linkedin.com/in/erick-bey/' target='_blank' rel="noreferrer"><BsLinkedin /></a>
+        <a href='https://github.com/erickbey' target='_blank' rel="noreferrer"><BsGithub /></a>
+      </div>
+
+      <div className="footer__copyright">
+        <small>&copy; Erick Bey. All Rights Reserved</small>
+      </div>
+    </footer>
   )
 }
 
