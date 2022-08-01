40a4c1bc1389848477e59e9797ce0da1c892c907
Erick Bey <erickbey10@gmail.com>
2022-08-01 01:05:59 -0400

    fixed a href error

--- src/component/Footer/Footer.jsx
+++ src/component/Footer/Footer.jsx
@@ -6,10 +6,10 @@ import { BsGithub } from 'react-icons/bs'
 function Footer() {
   return (
     <footer>
-      <a href="#" className="footer__logo">Erick Bey</a>
+      <a href="/#" className="footer__logo">Erick Bey</a>
 
       <ul className="permalinks">
-        <li><a href="#">Home</a></li>
+        <li><a href="/#">Home</a></li>
         <li><a href="#about">About</a></li>
         <li><a href="#experience">Experience</a></li>
         <li><a href="#projects">Projects</a></li>
